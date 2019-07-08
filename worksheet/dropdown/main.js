c9bca49c98285d9b936a6ac3d044836d424f6fc0
Ramiz.sbai <ramizsbai@hotmail.com>
2019-07-08 12:10:31 +0200

    dropdown nav is created

---
+++ worksheet/dropdown/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+
+    $('ul.parent > li').hover(function() {
+        $(this).find('ul.child').show(200);
+    }, function() {
+        $(this).find('ul.child').hide(200);
+    });
+});
